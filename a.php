ea71ad73fc3b0bd30b66cc4e4661ccd88060e92a
Khoa Nguyen <khoa-ng112@outlook.com>
2021-07-15 00:00:30 +0300

    dd

--- a.php
+++ a.php
@@ -1,6 +1,44 @@
-<?php
-sss
-s
-s
-s
-s
+{
+  from: 'outbound@saladoar.com',
+  to: '"Khoa Nguyen" <khoa-ng112@outlook.com>',
+  subject: 'Published Publication',
+  html: '<div>A blueprint offer "GL_Offerer" is published to product</div><div>undefined/760c05a2-98c8-4c07-b608-dfbe329c2fb9</div>',
+  dkim: {
+    domainName: 'saladoar.com',
+    keySelector: 'sendmail',
+    privateKey: '\n' +
+      '-----BEGIN RSA PRIVATE KEY-----\n' +
+      'MIIEogIBAAKCAQEAxei3Dx/kXGCqaLPkYWTWx/NkyOsYcPFdDetc306SKnQPpTpZ\n' +
+      'cyqCSOkUx9iPNvCl5YcbV3jlEegqsVj+dRQdamgg/oPEFeAUGxadOE83m+W35Leu\n' +
+      'dkCwj7/I+f2aQRW+RA/oiqrQK7c5LEKM1nm7jYiKf9l58EimTgjefK2rvEVJT7Gi\n' +
+      'l9+ZMAJu/WuNG6sZZbKPKwhQ42FM9wPYD4ifGlAYwSqTjPU11qo09po8trXWjxK1\n' +
+      'CjYGwhahsoiFJIuZ+NFUeGBGUq6bM73ZtUV4v/ygK5/SRx+F6Jo8pV4l3Gn83QmJ\n' +
+      'NWBbzDQ7PGg/RDHUcU9htyWPxlHARp+J29i6TQIDAQABAoIBAA1SA3guIlWhX/ba\n' +
+      'HMX9XoNou2DWifeYFzZHoCGbjTT0SNVLHxgkRU6Y+YAV8FW3TsMrG8kAZxKUeMMl\n' +
+      'BUNmDjDKP2ZCAiGiylekZwKSOmDwzjbDsiddZUTXmLRSwF4fE4ZLN9CVfJtkoqF6\n' +
+      '/TW2TcxR41iFj7J/gUoBSNBFdCigttM6b0UIkyXHwdyWPh3WgMn86S3dLqA41eoa\n' +
+      'l1jTB2q1KeBq4pPutz1udHYfCoRal9e1veu1zAPcr6FjHan9T/xw9Wnqt6XXr29S\n' +
+      '5YM4GOsfqb2v40sNULVDEt4XfLqZCxc4jmqy/sPV9TFGn9Nz1I945jTg0GEP2Zeg\n' +
+      'hb8BTwECgYEA8nrtbUuuZv9Aaz2RFQkoQXWy//EhM8ItID0llptIK/9nAzdoPxaG\n' +
+      '50NZMjFZghBOL5cA0zZQNquRPX60zNhoptDnYXPmT6GsrMI8qFztvyf0q+AUsEiC\n' +
+      'Rk/2R/m/pxK9GKIgW2cnWGrl8sKAktgourYXE/SiC32inNpABXxadY0CgYEA0PGY\n' +
+      'ekawDcfkGl0geoFTRmkyRhzr5wQZwpSUSmz5sFlh4ffApaqlmx4KvAxgcCp9dm7H\n' +
+      'cUSt8H4ElE+GeFW2xVI50PUH8l09wE28HAn1K65mRZWP1bzhoS42uR13cmvI/9cb\n' +
+      'sSC6HjPv6eu5wrQjGafd9Y6R1j0t9/vzBlg8R8ECgYAxdsQiTJL6Lc29UqTa7rBI\n' +
+      'kjL+pPjJ7y13ayHU1D5nw0UHUfExFW5VlNaIN6rzrlHUwXAIpga88EjDi6pkMy0+\n' +
+      'AlWA+rybt/Zak5D+I17YZNUzjpWJ1HDG2R77VcYz1c2/TateGe/byIq599V0nyXH\n' +
+      '2Mdiab/4qhtW9eqjc9ecLQKBgGp+qQOxoav0dOuHCbXG0qXxmiEoPohpi6OkTQy+\n' +
+      'DGl4tS4SM8t8dZnhLwF3oXOqBHZnshmmMq1/3V+mRNhyf4yFA1jgeuNp6L5qIHV+\n' +
+      'EcrowdG3kC8VbT7Uqimhvi7Ahxr3DXMzRgl82Uw1zGFTpQw/hh7RQr2aQyLA9LYI\n' +
+      'JPlBAoGAbbF5mI+ODPvIUKRj46Yr21vcRunhNxk+LQkHICo4kOtQZI4z5je+S+Sf\n' +
+      'mpdBcJVvMWY93ZHHAqpaYEbgTabFLhSUxwQVT0S7kQz+mY/1wVRtvpYYhMTPyeOE\n' +
+      'YtUd870aUdqVG28C2Tfpt3dnlwPuE+QSg7sw9pkwPHV+la8mgoc=\n' +
+      '-----END RSA PRIVATE KEY-----'
+  },
+  list: {
+    unsubscribe: {
+      url: 'undefined/760c05a2-98c8-4c07-b608-dfbe329c2fb9',
+      comment: 'Please click if you want to unsubscribe the email'
+    }
+  }
+}
